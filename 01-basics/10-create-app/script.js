92cea8c1dddd783d0aa93700c6653ff7ac519d48
User <gorbatovskiy.igor@gmail.com>
2024-10-02 20:57:39 +0300

    fix: create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -2,8 +2,15 @@ import { defineComponent, createApp } from 'vue'
 
 const App = defineComponent({
   name: 'DateString',
-  setup() {},
-  template: `<div>Сегодня ${new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })}</div>`,
+  setup() {
+    let currentDate = new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })
+
+    return {
+      currentDate,
+    }
+  },
+
+  template: `<div>Сегодня {{ currentDate }}</div>`,
 })
 
 createApp(App).mount('#app')
